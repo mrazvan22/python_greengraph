30bf609c0d0d199b013f0b32c9f26bfa71b375cd
Razvan Marinescu <rvm10@imperial.ac.uk>
2014-10-26 16:35:13 +0000

    added more tests

--- greengraph/test/test_greengraph.py
+++ greengraph/test/test_greengraph.py
@@ -11,6 +11,19 @@ def test_geolocate_1():
 
   assert_almost_equal(sum(london_location - np.array([51.5073509, -0.1277583])), 0.0)
 
+def test_is_green_1():
+  assert(is_green(0,1,0))
+
+def test_linespace():
+  start = np.array((0,0))
+  end = np.array((4,4))
+  steps = 3
+  points = np.array(location_sequence(start, end, steps))
+
+  diff = points - np.array([(0,0), (2,2), (4,4)])
+
+  assert(np.count_nonzero(diff) == 0)
+
 
 
 

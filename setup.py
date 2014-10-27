43f109841348eb2cb6dea2505adc535dc732bab7
Razvan Valentin Marinescu <mrazvan22@gmail.com>
2014-10-27 10:29:39 +0000

    added dependencies

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version = "0.1",
     packages = find_packages(exclude=['*test']),
     scripts = ['scripts/greengraph'],
-    install_requires = ['argparse']
+    install_requires = ['argparse', 'pypng', 'geopy']
 )
